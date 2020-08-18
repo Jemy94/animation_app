a5bf57dc5a44d998b26dc1d34fe36523c4dcc333
Jemy94 <ahmed.gamal9496@yahoo.com>
2020-08-18 14:51:29 +0200

    animation app using android property animation

---
+++ app/src/main/java/com/jemy/animationapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.jemy.animationapp
+
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatImageView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startVerticalAnimation()
+        startHorizontalAnimation()
+    }
+
+    private fun throwTheStarVertical() {
+        val container = star.parent as ViewGroup
+        val containerW = container.width
+        val containerH = container.height
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+        val starW: Float = star.width.toFloat()
+        val starH: Float = star.height.toFloat()
+        container.addView(newStar)
+        newStar.translationX = (containerW / 2f) - (starW / 2f)
+        val verticalAnimate = ObjectAnimator.ofFloat(
+            newStar, View.TRANSLATION_Y,
+            -starH, (containerH / 2.0f) - (starH / 2f)
+        )
+        verticalAnimate.duration = 4500
+        val rotate = ObjectAnimator.ofFloat(
+            newStar, View.ROTATION,
+            -1800f, 0f
+        )
+        rotate.duration = 12000
+        verticalAnimate.start()
+        rotate.start()
+    }
+
+    private fun throwTheStarHorizontal() {
+        val container = star.parent as ViewGroup
+        val containerW = container.width
+        val containerH = container.height
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+        val starWidth: Float = star.width.toFloat()
+        val starHeight: Float = star.height.toFloat()
+        container.addView(newStar)
+        newStar.translationY = (containerH / 2f) - (starHeight / 2f)
+        val horizontalAnimate = ObjectAnimator.ofFloat(
+            newStar, View.TRANSLATION_X,
+            -starWidth, (containerW / 2.0f) - (starWidth / 2f)
+        )
+        horizontalAnimate.duration = 2500
+        val rotate = ObjectAnimator.ofFloat(
+            newStar, View.ROTATION,
+            -1800f, 0f
+        )
+        rotate.duration = 12000
+        horizontalAnimate.start()
+        rotate.start()
+    }
+
+    private fun startVerticalAnimation() {
+        verticalAnimateButton.setOnClickListener {
+            throwTheStarVertical()
+        }
+    }
+
+    private fun startHorizontalAnimation() {
+        horizontalAnimateButton.setOnClickListener {
+            throwTheStarHorizontal()
+        }
+    }
+}
